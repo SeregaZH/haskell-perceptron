a4965d06cac5e542032d75c2217d87ef3163d918
SeregaZH <zhelezko1990@gmail.com>
2017-08-06 22:59:35 +0300

    implement basic perceptron functions

--- perceptron/app/Main.hs
+++ perceptron/app/Main.hs
@@ -4,4 +4,4 @@ import Lib
 import Perceptron
 
 main :: IO ()
-main = putStr $ show Perceptron { weight = [1, 2, 3] }
+main = putStr.show $ compute Perceptron { weights = [1, 2, 3], threshold = sigmoid } [2,3,4]
--- perceptron/src/Perceptron.hs
+++ perceptron/src/Perceptron.hs
@@ -1,6 +1,15 @@
 module Perceptron
     (
-        Perceptron(..)
+        Perceptron(..),
+        compute,
+        sigmoid,
+        correct
     ) where
 
-data Perceptron = Perceptron { weight :: [Integer] } deriving(Show)
+data Perceptron = Perceptron { weights :: [Double], threshold :: Double -> Double }
+compute :: Perceptron -> [Double] -> Double
+compute (Perceptron weights threshold) inputs = threshold $ foldl (+) 0.0 (zipWith (*) weights inputs)
+correct :: Perceptron -> Double -> Perceptron
+correct (Perceptron weights threshold) delta = Perceptron { weights = map ((*) delta) weights, threshold = threshold}
+sigmoid :: (Floating a) => a -> a
+sigmoid t = 1 / (1 + exp (-1 * t))
